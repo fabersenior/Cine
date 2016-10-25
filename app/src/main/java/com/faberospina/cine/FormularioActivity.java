138adb13adcc32e0bbc89654903f15e86ffbf5e2
Faber <faber_729@hotmail.com>
2016-10-25 17:01:17 -0500

    Cine Nav Catalogo-show-resumen-pago

--- app/src/main/java/com/faberospina/cine/FormularioActivity.java
+++ app/src/main/java/com/faberospina/cine/FormularioActivity.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class FormaPagoActivity extends AppCompatActivity {
+public class FormularioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forma_pago);
+        setContentView(R.layout.activity_formulario);
     }
-    public void btn_Continuar(View view){
-        Intent intent= new Intent(getApplicationContext(),FormularioActivity.class);
+    public void btn_Comprar(View view){
+        Intent intent= new Intent(getApplicationContext(),CatalogoActivity.class);
         startActivity(intent);
     }
 }
